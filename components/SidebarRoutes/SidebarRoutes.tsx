6d6dd98f3b0ec20bdc4d1ed4063b7c887da1b228
Juan Cruz Montecino <montecinoruizj@gmail.com>
2025-02-20 20:52:03 -0300

    navbar

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,17 +1,52 @@
+"use client"
+
+import { SidebarItem } from "../SidebarItem"
+import { Button } from "../ui/button"
+import { Separator } from "../ui/separator"
 import { dataGeneralSidebar, dataSupportSidebar, dataToolSidebar } from "./SidebarRoutes.data"
 
 export function SidebarRoutes() {
   return (
     <div className="flex flex-col justify-between h-full">
         <div>
+
             <div className="p-2 md:p-6">
-                <p>general</p>
-                {dataGeneralSidebar.map((item) => (
-                    
-                  <p key={item.label}>{item.label}</p>
-                    
+              <p className="text-slate-500 mb-2">General</p>
+              {dataGeneralSidebar.map((item) => (
+                <SidebarItem key={item.label} item={item}/>
                 ))}
             </div>
+
+          <Separator />
+
+            <div className="p-2 md:p-6">
+              <p className="text-slate-500 mb-2" >Tools</p>
+              {dataToolSidebar.map((item) => (
+                      <SidebarItem key={item.label} item={item}/>
+              ))}
+            </div>
+
+          <Separator />
+
+            <div className="p-2 md:p-6">
+                <p className="text-slate-500 mb-2">Support</p>
+                {dataSupportSidebar.map((item) => (
+                      <SidebarItem key={item.label} item={item}/>
+                ))} 
+            </div>
+        </div>
+
+        <div>
+          <div className="text-center p-6">
+                <Button variant={"outline"} className="w-full">
+                  Upgrade Plan
+                  </Button>
+          </div>
+          <Separator />
+
+          <footer className="text-center p-3 mt-3">
+                Todos los derechos reservados | 2025
+            </footer>
         </div>
     </div>
   )
